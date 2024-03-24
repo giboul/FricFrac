e7b6a89c22d01906e81bda0b1ee6a2320a6989e6
giboul <axel.giboulot@epfl.ch>
2024-03-24 13:42:36 +0100

    utils.py works !

--- utils.py
+++ utils.py
@@ -1,152 +1,87 @@
-from matplotlib import pyplot as plt
-from matplotlib.figure import Figure
-from typing import Tuple, NamedTuple
+"""Module for analyzing fric-frac's results"""
 import numpy as np
+import pandas as pd
+from numpy.typing import ArrayLike, NDArray
+from matplotlib import pyplot as plt
 
 
-def wheatstone(R1: float, R2: float, R3: float, Vin: float, Vout: np.ndarray) -> np.ndarray:
-    """Solve for the value of the unknown resistance R3
-    https://en.wikipedia.org/wiki/Wheatstone_bridge"""
-    return R3 * (Vin*(R1+R2)/(Vin*R1 - Vout*(R1+R2)) - 1)
+def rotation_matrix(angles: ArrayLike) -> NDArray:
+    """Rotate stress field by `theta` degrees to be aligned to fric-frac
+    s = s_xx*cos(theta)² + s_yy*sin(theta)² + s_xy*sin(2theta)"""
+    a, b, c = angles
+    return np.array([
+        [np.sin(b)*np.sin(c)/(np.sin(a-b)*np.sin(a-c)), -np.sin(a)*np.sin(c)/(np.sin(a-b)*np.sin(b-c)), np.sin(a)*np.sin(b)/(np.sin(a-c)*np.sin(b-c))],
+        [np.cos(b)*np.cos(c)/(np.sin(a-b)*np.sin(a-c)), -np.cos(a)*np.cos(c)/(np.sin(a-b)*np.sin(b-c)), np.cos(a)*np.cos(b)/(np.sin(a-c)*np.sin(b-c))],
+        [-np.sin(b+c)/(np.sin(a-b)*np.sin(a-c))/2, np.sin(a+c)/(np.sin(a-b)*np.sin(b-c))/2, -np.sin(a+b)/(np.sin(a-c)*np.sin(b-c))/2],
+    ])
 
 
-def cable_deformation(l0: float, S0: float, rho: float, dR: np.ndarray) -> np.ndarray:
-    """Computes the deformation 'ε' of a cable from its resistance variation, initial geometry (l0, S0) and resistivity 'rho'"""
-    return np.sqrt(l0**2 + dR*S0*l0/rho) / l0 - 1
+def plane_stress_matrix(E: float, nu: float) -> NDArray:
+    return E / (1-nu**2) * np.array([
+        [1, nu, 0],
+        [nu, 1, 0],
+        [0, 0, (1-nu)]
+    ])
 
 
-class Wheatstone(NamedTuple):
-    R1: float
-    R2: float
-    R3: float
-    Vin: float
+def hooke(E: float, nu: float, strains: NDArray) -> NDArray:
+    mu = E/(2*(1+nu))
+    lambd = E*nu/((1+nu)*(1-2*nu))
+    trace = np.trace(strains)
+    strains = strains.T.reshape((-1, 3, 3), order='F')
+    z = np.zeros(strains.shape[0])
+    trace = np.array([
+        [trace, z, z],
+        [z, trace, z],
+        [z, z, trace]
+    ]).T.reshape((-1, 3, 3), order='F')
+    return (2*mu*strains + lambd * trace).T.reshape((3, 3, -1), order='F')
 
 
-class Cable(NamedTuple):
-    l0: float
-    S0: float
-    rho: float
+def main():
+    """mate this is main"""
+    ampli = -5000e-6
+    E = 2.59e9
+    nu = 0.35
+    df = pd.read_csv("load_test_000.csv", skiprows=7, delimiter=";")
+    # df = df.rolling(10).mean()
+    time = df["Relative time"]
+    ng = len(df.columns)//3
 
+    fig, axes = plt.subplots(nrows=3, ncols=ng, sharex=True, sharey='row', gridspec_kw=dict(hspace=0, wspace=0))
 
-class Hooke(NamedTuple):
-    E: float
-    nu: float
+    for gauge, axcol in enumerate(axes.T, start=1):
+        gauges = [f"Ch{4*(gauge-1)+1:0>2}",f"Ch{4*(gauge-1)+2:0>2}",f"Ch{4*(gauge-1)+3:0>2}"]
+        print(f"Processing gauges {gauges} {gauge}/{ng} ({gauge/ng:.0%})", end='\r')
+        data = df[gauges].to_numpy().T
 
-    def __matmul__(self, strains: np.ndarray) -> np.ndarray:
-        lambd = self.E*self.nu/(1+self.nu)/(1-2*self.nu)
-        mu = self.E/(1+self.nu)
-        strain = strains.reshape(-1, 3, 3)
-        stresses = lambd * np.trace(strain) + 2*mu*strain
-        return stresses.reshape(3, 3, -1)
-
-
-class Gauje:
-    bridge: Wheatstone
-    cable: Cable
-
-    def __init__(self, bridge: Wheatstone, cable: Cable) -> None:
-        self.bridge = bridge
-        self.cable = cable
-
-    def dRx(self, Vout: np.ndarray) -> np.ndarray:
-        return wheatstone(*self.bridge, Vout) - self.bridge.R3
-
-    def deformation(self, dR: np.ndarray) -> np.ndarray:
-        return cable_deformation(*self.cable, dR)
-
-
-class Rosette:
-    gauges: Tuple[Gauje]
-    claw: Hooke
-
-    def __init__(self, gauge1: Gauje, gauge2: Gauje, gauge3: Gauje, claw: Hooke) -> None:
-        self.gauges = gauge1, gauge2, gauge3
-        self.claw = claw
-
-    @staticmethod
-    def strains(dl1: np.ndarray, dl2: np.ndarray, dl3: np.ndarray, plane='stress') -> np.ndarray:
-        e11 = dl1 + dl3 - dl2
-        e12 = dl1 - dl3
-        e22 = dl2
-        zer = np.zeros(e11.size)
-        if plane == 'strain':  # Review this # TODO
-            strains2d = np.array([
-                [e11, e12, zer],
-                [e12, e22, zer],
-                [zer, zer, zer],
-            ])
-        elif plane == "stress":  # Review this # TODO
-            zer = np.zeros_like(e11)
-            strains2d = np.array([
-                [e11, e12, zer],
-                [e12, e22, zer],
-                [zer, zer, -e11-e22],
-            ])
-        else:
-            raise ValueError(f"Plane '{plane}' is not supported")
-        return strains2d
-
-    def stress(self, strains: np.ndarray) -> np.ndarray:
-        return self.claw @ strains
-
-    def BigBrother(self, f: float, V1: np.ndarray, V2: np.ndarray, V3: np.ndarray) -> Tuple[Figure, plt.Axes]:
-
-        R1, R2, R3 = self.gauges[0].dRx(V1), self.gauges[1].dRx(V2), self.gauges[2].dRx(V3)
-        dl1, dl2, dl3 = self.gauges[0].deformation(R1), self.gauges[1].deformation(R2), self.gauges[2].deformation(R3)
-        # strains = self.strains(dl1, dl2, dl3)
-        e1, e2, g = -5000 * 1e-6 * np.array((V1, V2, V3))
-        zeros = np.zeros(V1.size)
-        strains = np.array([
-            [e1, g, zeros],
-            [g, e2, zeros],
-            [zeros, zeros, -e1-e2]
+        e11, e22, e12 = rotation_matrix(np.deg2rad((45, 90, 135))) @ (ampli * data)
+        strains_matrix = np.array([
+            [e11, e12, 0*e11],
+            [e12, e22, 0*e11],
+            [0*e11, 0*e11, -nu/(1-nu)*(e11+e22)]
         ])
-        stresses = self.claw @ strains
+        stresses_matrix = hooke(E, nu, strains_matrix)
+        strains = strains_matrix[0, 0, :], strains_matrix[1, 1, :], 2*strains_matrix[0, 1, :]
+        stresses = stresses_matrix[0, 0], stresses_matrix[1, 1], stresses_matrix[0, 1]
 
-        strains = strains[0, 0], strains[0, 1], strains[1, 1]
-        stresses = stresses[0, 0], stresses[0, 1], stresses[1, 1]
+        print("Plotting gauge   ", end='\r')
+        for lab, labb, pot, strain, stress in zip((1, 2, 3), (11, 22, 12), data, strains, stresses):
+            axcol[0].set_title(f"Rosette {gauge}")
+            axcol[0].plot(time/60, pot, label=rf"$\Delta R_{{{lab}}}$")
+            axcol[1].plot(time/60, strain*100, label=rf"$\varepsilon_{{{labb}}}$")
+            axcol[2].plot(time/60, stress/1e6, label=rf"$\sigma_{{{labb}}}$")
+    print()
+    axes[0, 0].legend(loc='upper left')
+    axes[1, 0].legend(loc='upper left')
+    axes[2, 0].legend(loc='upper left')
+    axes[0, 0].set_ylabel('Résistance [ohm]')
+    axes[1, 0].set_ylabel('Déformation [%]')
+    axes[2, 0].set_ylabel('Contrainte [MPa]')
+    axes[2, 0].set_xlabel('temps [min]')
+    plt.show()
 
-        t = np.arange(0, V1.size) / f
-
-        fig, axes = plt.subplots()
-        plt.plot(t, stresses[0], label = r"$\sigma_{{11}}$")
-        plt.plot(t, stresses[1], label = r"$\sigma_{{12}}$")
-        plt.plot(t, stresses[2], label = r"$\sigma_{{22}}$")
-        plt.legend()
-        plt.xlabel("t [s]")
-
-        # fig, axes = plt.subplots(nrows=3, ncols=3, sharex=True)
-
-        # for axs, lab, v, r, dl, sn, ss in zip(axes, (11, 12, 22), (V1, V2, V3), (R1, R2, R3), (dl1, dl2, dl3), strains, stresses):
-        #     axV, axsn, axss = axs
-        #     axV.set_ylabel(f"$V_{{G{lab}}}$")
-        #     # axR.set_ylabel(rf"$\Delta R_{{x{lab}}}$")
-        #     # axl.set_ylabel(rf"$\Delta\ell_{{{lab}}}$")
-        #     axsn.set_ylabel(rf"$\varepsilon_{{{lab}}}$")
-        #     axss.set_ylabel(rf"$\sigma_{{{lab}}}$")
-        #     axV.plot(t, v)
-        #     # axR.plot(t, r)
-        #     # axl.plot(t, dl)
-        #     axsn.plot(t, sn)
-        #     axss.plot(t, ss)
-        # for ax in axs:
-        #     ax.set_xlabel("$t$ [s]")
-        # for axs in axes[:-1]:
-        #     for ax in axs:
-        #         ax.set_xticklabels([])
-
-        return fig, axes
 
 if __name__ == "__main__":
-
-    Vin, V1, V2, V3 = np.loadtxt("Ulog.txt", skiprows=1).T
-
-    bridge = Wheatstone(120, 120, 120, Vin)
-    cable = Cable(1e6, 1e-4, 10e-6)
-    claw = Hooke(3e9, 0.4)
-    gauge = Gauje(bridge, cable)
-    rosette = Rosette(gauge, gauge, gauge, claw)
-
-    with plt.style.context('ggplot'):
-        rosette.BigBrother(10e6, V1, V2, V3)
-        plt.show()
+    with plt.style.context("ggplot"):
+        main()
