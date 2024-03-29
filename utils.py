586a5df2ff383a548b79d0a7847114f7d74dd174
giboul <axel.giboulot@epfl.ch>
2024-03-29 20:50:22 +0100

    Mean Brother is born

--- utils.py
+++ utils.py
@@ -39,7 +39,7 @@ def hooke(E: float, nu: float, strains: NDArray) -> NDArray:
     return (2*mu*strains + lambd * trace).T.reshape((3, 3, -1), order='F')
 
 
-def main():
+def BigBrother():
     """mate this is main"""
     ampli = -5000e-6
     E = 2.59e9
@@ -94,7 +94,57 @@ def main():
     axes[2, 0].set_xlabel('temps [min]')
     plt.show()
 
+def MeanBrother():
+    """mate this is main"""
+    ampli = -5000e-6
+    E = 2.59e9
+    nu = 0.35
+    file = "load_test_000.csv"
+    cols = pl.read_csv(file, separator=";", skip_rows=7, n_rows=1).columns
+    df = pl.read_csv(file, separator=";", skip_rows=9, new_columns=cols)
+    for col in cols[1:]:
+        fs = 1/np.diff(df['Relative time']).mean()
+        sos = butter(1, 1.0, 'lowpass', fs=fs, output='sos')
+        df = df.with_columns(**{col: sosfilt(sos, df[col])})
+    time = df["Relative time"]
+    ng = len(df.columns)//3
+
+    mean_gauges = [f"Ch{4*ng+i:0>2}" for i in range(1,4)]
+    df = df.with_columns(**{
+        k: pl.sum_horizontal(*[f"Ch{4*g+i:0>2}" for g in range(ng)])/ng
+        for i, k in enumerate(mean_gauges, start=1)
+    })
+    print(df)
+
+    data = df[mean_gauges].to_numpy().T
+    e11, e22, e12 = rotation_matrix(np.deg2rad((45, 90, 135))) @ (ampli * data)
+    strains_matrix = np.array([
+        [e11, e12, 0*e11],
+        [e12, e22, 0*e11],
+        [0*e11, 0*e11, -nu/(1-nu)*(e11+e22)]
+    ])
+    stresses_matrix = hooke(E, nu, strains_matrix)
+    strains = strains_matrix[0, 0], strains_matrix[1, 1], np.abs(2*strains_matrix[0, 1])
+    stresses = stresses_matrix[0, 0], stresses_matrix[1, 1], np.abs(stresses_matrix[0, 1])
+
+    fig, (ax1, ax2, ax3) = plt.subplots(nrows=3)
+
+    for lab, labb, pot, strain, stress in zip((1, 2, 3), (11, 22, 12), data, strains, stresses):
+        ax1.plot(time/60, pot, label=rf"$\Delta R_{{{lab}}}$")
+        ax2.plot(time/60, strain*100, label=rf"$\varepsilon_{{{labb}}}$")
+        ax3.plot(time/60, stress/1e6, label=rf"$\sigma_{{{labb}}}$")
+    print()
+    ax1.set_title("Moyenne")
+    ax1.legend(loc='upper left')
+    ax2.legend(loc='upper left')
+    ax3.legend(loc='upper left')
+    ax1.set_ylabel('Résistance [ohm]')
+    ax2.set_ylabel('Déformation [%]')
+    ax3.set_ylabel('Contrainte [MPa]')
+    ax3.set_xlabel('temps [min]')
+    plt.show()
 
 if __name__ == "__main__":
     with plt.style.context("ggplot"):
-        main()
+        # BigBrother()
+        MeanBrother()
