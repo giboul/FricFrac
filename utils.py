fd94e3353674a397b45aec56004c14c85816b3cd
giboul <axel.giboulot@epfl.ch>
2024-04-12 20:22:02 +0200

    added vertical lines on click

--- utils.py
+++ utils.py
@@ -1,9 +1,16 @@
 """Module for analyzing fric-frac's results"""
 import numpy as np
 import polars as pl
+import pandas as pd
 from scipy.signal import butter, sosfilt
 from numpy.typing import ArrayLike, NDArray
 from matplotlib import pyplot as plt
+from pathlib import Path
+from tkinter import Tk, filedialog
+
+
+class Temp:
+    pass
 
 
 def rotation_matrix(angles: ArrayLike) -> NDArray:
@@ -39,29 +46,117 @@ def hooke(E: float, nu: float, strains: NDArray) -> NDArray:
     return (2*mu*strains + lambd * trace).T.reshape((3, 3, -1), order='F')
 
 
-def BigBrother():
-    """mate this is main"""
-    ampli = -5000e-6
-    E = 2.59e9
-    nu = 0.35
-    file = "load_test_000.csv"
-    cols = pl.read_csv(file, separator=";", skip_rows=7, n_rows=1).columns
-    df = pl.read_csv(file, separator=";", skip_rows=9, new_columns=cols)
+def MeanBrother(file, polars=False):
+
+    if polars:
+        cols = pl.read_csv(file, separator=";", skip_rows=7, n_rows=1).columns
+        df = pl.read_csv(file, separator=";", skip_rows=9, new_columns=cols, infer_schema_length=None)
+    else:
+        cols = pd.read_csv(file, sep=";", skiprows=7, nrows=1).columns
+        df = pd.read_csv(file, sep=";", skiprows=9, names=cols)
+
     for col in cols[1:]:
         fs = 1/np.diff(df['Relative time']).mean()
         sos = butter(1, 1.0, 'lowpass', fs=fs, output='sos')
-        df = df.with_columns(**{col: sosfilt(sos, df[col])})
+        # df = df.with_columns(**{col: sosfilt(sos, df[col])})
+        df[col] = sosfilt(sos, df[col])
     time = df["Relative time"]
     ng = len(df.columns)//3
 
-    df = df.with_columns(**{
-        k: pl.sum_horizontal(*[f"Ch{4*g+i:0>2}" for g in range(ng)])/ng
-        for i, k in enumerate([f"Ch{4*ng+i:0>2}" for i in range(1,4)], start=1)
-    })
+    mean_gauges = [f"Ch{4*ng+i:0>2}" for i in range(1, 4)]
+    if polars:
+        df = df.with_columns(**{
+            k: pl.sum_horizontal(*[f"Ch{4*g+i:0>2}" for g in range(ng)])/ng
+            for i, k in enumerate(mean_gauges, start=1)
+        })
+    else:
+        for i, k in enumerate(mean_gauges, start=1):
+            cols = [f"Ch{4*g+i:0>2}" for g in range(ng)]
+            df[k] = df[cols].mean(axis=1)
     print(df)
 
-    fig, axes = plt.subplots(nrows=3, ncols=ng+1, sharex=True, sharey='row', gridspec_kw=dict(hspace=0, wspace=0))
+    data = df[mean_gauges].to_numpy().T
+    e11, e22, e12 = rotation_matrix(np.deg2rad((45, 90, 135))) @ (ampli * data)
+    strains_matrix = np.array([
+        [e11, e12, 0*e11],
+        [e12, e22, 0*e11],
+        [0*e11, 0*e11, -nu/(1-nu)*(e11+e22)]
+    ])
+    stresses_matrix = hooke(E, nu, strains_matrix)
+    strains = strains_matrix[0, 0], strains_matrix[1, 1], np.abs(2*strains_matrix[0, 1])
+    stresses = stresses_matrix[0, 0], stresses_matrix[1, 1], np.abs(stresses_matrix[0, 1])
 
+    fig, axes = plt.subplots(nrows=3)
+    ax1, ax2, ax3 = axes
+    fig.suptitle(file.stem)
+
+    for lab, labb, pot, strain, stress in zip((1, 2, 3), (11, 22, 12), data, strains, stresses):
+        ax1.plot(time/60, pot, label=rf"$\Delta R_{{{lab}}}$")
+        ax2.plot(time/60, strain*100, label=rf"$\varepsilon_{{{labb}}}$")
+        ax3.plot(time/60, stress/1e6, label=rf"$\sigma_{{{labb}}}$")
+    print()
+    ax1.set_title("Moyenne")
+    ax1.legend(loc='upper left')
+    ax2.legend(loc='upper left')
+    ax3.legend(loc='upper left')
+    ax1.set_ylabel('Résistance [ohm]')
+    ax2.set_ylabel('Déformation [%]')
+    ax3.set_ylabel('Contrainte [MPa]')
+    ax3.set_xlabel('temps [min]')
+    return fig, axes
+
+
+def BigBrother(file, polars=False):
+
+    # Reading files
+    if polars:
+        cols = pl.read_csv(file, separator=";", skip_rows=7, n_rows=1).columns
+        df = pl.read_csv(file, separator=";", skip_rows=9, new_columns=cols, infer_schema_length=None)
+    else:
+        cols = pd.read_csv(file, sep=";", skiprows=7, nrows=1).columns
+        df = pd.read_csv(file, sep=";", skiprows=9, names=cols)
+
+    # Filtering
+    # for col in cols[1:]:
+    #     fs = 1/np.diff(df['Relative time']).mean()
+    #     sos = butter(1, 1.0, 'lowpass', fs=fs, output='sos')
+    #     # df = df.with_columns(**{col: sosfilt(sos, df[col])})
+    #     df[col] = sosfilt(sos, df[col])
+
+    time = df["Relative time"]
+    ng = len(df.columns)//3
+
+    # Adding columns for mean values
+    mean_gauges = [f"Ch{4*ng+i:0>2}" for i in range(1, 4)]
+    if polars:
+        df = df.with_columns(**{
+            k: pl.sum_horizontal(*[f"Ch{4*g+i:0>2}" for g in range(ng)])/ng
+            for i, k in enumerate(mean_gauges, start=1)
+        })
+    else:
+        for i, k in enumerate(mean_gauges, start=1):
+            cols = [f"Ch{4*g+i:0>2}" for g in range(ng)]
+            df[k] = df[cols].mean(axis=1)
+    print(df)
+
+    # Figure definition
+    fig, axes = plt.subplots(nrows=3, ncols=ng+1, sharex=True, sharey='row', gridspec_kw=dict(hspace=0, wspace=0))
+    fig.suptitle(file.stem)
+    # Vertical lines
+    axlines = [
+        [ax.axvline(0., ls='-.', c='gray') for ax in axes[i]]
+        for i in range(len(axes))
+    ]
+    def onclick_GridUpdate(event):
+        x, y = event.xdata, event.ydata
+        for alrow in axlines:
+            for al in alrow:
+                al.set_xdata(np.full_like(al.get_xdata(), x))
+        fig.suptitle(f"{file.stem}: {x=:.2e}")
+        fig.canvas.draw()
+    fig.canvas.mpl_connect('button_press_event', onclick_GridUpdate)
+
+    # Plotting each gauge
     for gauge, axcol in enumerate(axes.T):
         gauges = [f"Ch{4*gauge+1:0>2}",f"Ch{4*gauge+2:0>2}",f"Ch{4*gauge+3:0>2}"]
         print(f"Processing gauges {gauges} {gauge}/{ng} ({gauge/ng:.0%})", end='\r')
@@ -92,59 +187,28 @@ def BigBrother():
     axes[1, 0].set_ylabel('Déformation [%]')
     axes[2, 0].set_ylabel('Contrainte [MPa]')
     axes[2, 0].set_xlabel('temps [min]')
-    plt.show()
+    return fig, axes
 
-def MeanBrother():
-    """mate this is main"""
+
+def main(*args, **kwargs):
+    # Get file paths
+    Tk().withdraw()
+    directory = Path(filedialog.askdirectory())
+    files = filedialog.askopenfilenames(
+        initialdir=directory,
+        filetypes=[('Comma Separated Values', '.csv')]
+    )
+    files = [directory/f for f in files]
+    # Plot
+    with plt.style.context("ggplot"):
+        for file in files:
+            BigBrother(file, *args, **kwargs)
+            # MeanBrother(file, *args, **kwargs)
+            plt.show()
+
+
+if __name__ == "__main__":
     ampli = -5000e-6
     E = 2.59e9
     nu = 0.35
-    file = "load_test_000.csv"
-    cols = pl.read_csv(file, separator=";", skip_rows=7, n_rows=1).columns
-    df = pl.read_csv(file, separator=";", skip_rows=9, new_columns=cols)
-    for col in cols[1:]:
-        fs = 1/np.diff(df['Relative time']).mean()
-        sos = butter(1, 1.0, 'lowpass', fs=fs, output='sos')
-        df = df.with_columns(**{col: sosfilt(sos, df[col])})
-    time = df["Relative time"]
-    ng = len(df.columns)//3
-
-    mean_gauges = [f"Ch{4*ng+i:0>2}" for i in range(1,4)]
-    df = df.with_columns(**{
-        k: pl.sum_horizontal(*[f"Ch{4*g+i:0>2}" for g in range(ng)])/ng
-        for i, k in enumerate(mean_gauges, start=1)
-    })
-    print(df)
-
-    data = df[mean_gauges].to_numpy().T
-    e11, e22, e12 = rotation_matrix(np.deg2rad((45, 90, 135))) @ (ampli * data)
-    strains_matrix = np.array([
-        [e11, e12, 0*e11],
-        [e12, e22, 0*e11],
-        [0*e11, 0*e11, -nu/(1-nu)*(e11+e22)]
-    ])
-    stresses_matrix = hooke(E, nu, strains_matrix)
-    strains = strains_matrix[0, 0], strains_matrix[1, 1], np.abs(2*strains_matrix[0, 1])
-    stresses = stresses_matrix[0, 0], stresses_matrix[1, 1], np.abs(stresses_matrix[0, 1])
-
-    fig, (ax1, ax2, ax3) = plt.subplots(nrows=3)
-
-    for lab, labb, pot, strain, stress in zip((1, 2, 3), (11, 22, 12), data, strains, stresses):
-        ax1.plot(time/60, pot, label=rf"$\Delta R_{{{lab}}}$")
-        ax2.plot(time/60, strain*100, label=rf"$\varepsilon_{{{labb}}}$")
-        ax3.plot(time/60, stress/1e6, label=rf"$\sigma_{{{labb}}}$")
-    print()
-    ax1.set_title("Moyenne")
-    ax1.legend(loc='upper left')
-    ax2.legend(loc='upper left')
-    ax3.legend(loc='upper left')
-    ax1.set_ylabel('Résistance [ohm]')
-    ax2.set_ylabel('Déformation [%]')
-    ax3.set_ylabel('Contrainte [MPa]')
-    ax3.set_xlabel('temps [min]')
-    plt.show()
-
-if __name__ == "__main__":
-    with plt.style.context("ggplot"):
-        # BigBrother()
-        MeanBrother()
+    main(polars=True)
