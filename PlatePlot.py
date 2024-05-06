06e287091f7f6b713b1231b55b9d15e0eb1c0777
giboul <axel.giboulot@epfl.ch>
2024-05-06 10:49:41 +0200

    handy plotting

--- PlatePlot.py
+++ PlatePlot.py
@@ -9,53 +9,41 @@ from BigBrother import (
 import numpy as np
 
 
-def plot_loop():
-
-    keys = dict(index=0, step=1)
-    def onkey(event):
-        if event.key == 'right':
-            keys["index"] += keys['step']
-        elif event.key == 'left':
-            keys["index"] -= keys['step']
-        elif event.key == 'up':
-            keys['step'] += 1
-        elif event.key == 'down':
-            keys['step'] -= 1
-        elif event.key == 'pageup':
-            keys['step'] += 100
-        elif event.key == 'pagedown':
-            keys['step'] -= 100
-        else:
-            print(f"WARNING: key '{event.key}' not implemented")
-
-        data = [g["strains"][0][keys["index"]] for g in gauge_data]
-        print(data)
-        ax.set_title(title.format(keys['index']-1, keys['step']))
-        points.set_data(x_gauges, data)
-        ax.relim()
-        ax.autoscale_view()
-        fig.canvas.draw()
-
-    data = [g["strains"][0][keys["index"]] for g in gauge_data]
-    points, = ax.plot(x_gauges, data, '-o', mfc='w')
-    ax.set_title(title.format(keys['index']-1, keys['step']))
-    fig.canvas.mpl_connect('key_press_event', onkey)
-    plt.tight_layout()
-    plt.show()
+keys = dict(index=0, step=1)
+def update_keys(event):
+    if event.key == 'right':
+        keys["index"] = max(0, min(keys["index"]+keys['step'], time.size-1))
+        return True
+    elif event.key == 'left':
+        keys["index"] = max(0, min(keys["index"]-keys['step'], time.size-1))
+        return True
+    elif event.key == 'up':
+        keys['step'] += 10
+    elif event.key == 'down':
+        keys['step'] -= 10
+    elif event.key == 'pageup':
+        keys['step'] += 1000
+    elif event.key == 'pagedown':
+        keys['step'] -= 1000
+    else:
+        print(f"WARNING: key '{event.key}' not implemented")
+    return False
 
 
-x_gauges = np.cumsum((20, 30, 30, 30, 20))
+# Définition du problème
+x_gauges = np.cumsum((20, 60, 60, 60, 20))
 mat = Material(E=2.59e9, nu=0.35)
+ampli = -5000e-6
 
 kind_dict = dict(parallel=0, tangent=1, shear=2)
 
-#files = select_files()
+# files = select_files()
 files = ["data/240417/test2_001.csv"]
 for file in files:
     df = read_with_pandas(file, sep=";", skiprows=list(range(7))+[8], rolling=100).to_numpy().T
     # df = read_with_polars(file, separator=";", skip_rows=7, skip_rows_after_header=1, rolling=100).to_numpy().T
 
-    fig, ax = plt.subplots()
+    fig, (ax, axBB) = plt.subplots(nrows=2)
     title = (
         "Index: {}, Step: {}\n"
         r"advance $\rightarrow$       rewind $\leftarrow$""\n"
@@ -63,11 +51,53 @@ for file in files:
         r"step += 100 $\Uparrow$      step-=100$\Downarrow$"
     )
 
+    # Reading files
+    time = df[0]
     gauge_data = []
-    for gauge, x in enumerate(x_gauges):  # TODO
-        print(f"Treating gauge {gauge}/{len(x_gauges)}", end="\r")
-        strains, stresses = stress_strain(mat, df[3*gauge+1:3*gauge+4], -5000e-6)
-        gauge_data.append(dict(strains=strains, stresses=stresses))
+    for gauge, x in enumerate(x_gauges):
+        print(f"Treating gauge {gauge+1}/{len(x_gauges)}", end="\r")
+        strains, stresses = stress_strain(mat, df[3*gauge+1:3*gauge+4], ampli)
+        gauge_data.append(stresses)
+    gauge_data = np.array(gauge_data)
     print(f"\nDone")
 
-    plot_loop()
+    # merged MeanBrother plot
+    strains, stresses = stress_strain(mat, df[-3:], ampli)
+    for lab, stress in zip(("Parallel", "Normal", "Shear"), stresses):
+        axBB.plot(time, stress/1e6, label=lab)
+    axBB.set_xlabel("Time [s]")
+    axBB.set_ylabel(r"$\sigma$ [MPa]")
+    vline = axBB.axvline(time[0], ls='-.', c='gray', alpha=0.6)
+    axBB.legend(loc="upper left")
+
+    # Plot for each gauge
+    # First plot
+    points = []
+    for i, lab in enumerate(("Parallel", "Normal", "Shear")):
+        stresses = [g[i][keys["index"]] for g in gauge_data]
+        points_i, = ax.plot(x_gauges, stresses, '-o', mfc='w', label=lab)
+        points.append(points_i)
+
+    ax.set_title(title.format(keys['index'], keys['step']))
+    ax.set_xlabel("Position on plate")
+    ax.set_ylabel(rf"$\sigma(t = {time[keys['index']]:.2e} s)$")
+    ax.dataLim.y0 = gauge_data.min()
+    ax.dataLim.y1 = gauge_data.max()
+    # ax.legend(loc="upper right")
+
+    # Add key events
+    def onkey(event):
+        if update_keys(event):
+            for i, direction in enumerate(("Parallel", "Normal", "Shear")):
+                data = [g[i][keys["index"]] for g in gauge_data]
+                points[i].set_data(x_gauges, data)
+            vline.set_xdata(np.full_like(vline.get_xdata(), time[keys["index"]]))
+            ax.set_ylabel(rf"$\sigma(t = {time[keys['index']]:.2e} s)$")
+            # ax.relim()
+            # ax.autoscale_view()
+        ax.set_title(title.format(keys['index'], keys['step']))
+        fig.canvas.draw()
+    fig.canvas.mpl_connect('key_press_event', onkey)
+
+    plt.tight_layout()
+    plt.show()
