3b6ba8d69ea283688898f5c9141285e88813114d
giboul <axel.giboulot@epfl.ch>
2024-06-04 01:58:52 +0200

    useless

--- femplate.py
+++
@@ -1,4 +0,0 @@
-import matplotlib.pyplot as plt  # load matplotlib
-from solidspy import solids_GUI  # import our package
-disp = solids_GUI()  # run the Finite Element Analysis
-plt.show()    # plot contours
