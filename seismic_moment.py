c4c57a8761485a8ea093cf91681c90166ff6890e
giboul <axel.giboulot@epfl.ch>
2024-06-04 01:59:55 +0200

    correct computations

--- seismic_moment.py
+++ seismic_moment.py
@@ -1,11 +1,11 @@
 from numpy import log10
 # https://en.wikipedia.org/wiki/Seismic_moment
 mu = 0.95e9  # Shear module of PMMA
-width = 2.5  # m  # width of the plate
-thk = 0.05  # m  # thickness of the plate
-A = thk * width  # m²  # Area of fracture
-D = (6.48e-2 - 5.52e-2) * width
+l = 2.5  # m  # width of the plate
+thk = 0.03  # m  # thickness of the plate
+A = thk * l  # m²  # Area of fracture
+D = (0.1075 - 0.1)/100 * l
 M0 = mu*A*D
-Mw = 2/3 * log10(M0) - 10.7
+Mw = 2/3 * log10(M0) - 6.07
 print(f"{M0 = }")
 print(f"{Mw = }")
