507b15248c6f71f8d7284a831f2677de23395c67
giboul <axel.giboulot@epfl.ch>
2024-06-04 19:28:04 +0200

    generalization

--- PlatePlot.py
+++ PlatePlot.py
@@ -1,5 +1,4 @@
-"""Module for interactive analysis of the stresses across the plate"""
-
+"""Module for interactive analysis across the plate"""
 from matplotlib import pyplot as plt
 from pathlib import Path
 from BigBrother import (
@@ -33,7 +32,7 @@ def update_keys(event, redraw=False) -> bool:
     return redraw
 
 
-def plateplot(x_gauges, E, nu, ampli, file, timecol="Relative time"):
+def plateplot(df, x_gauges, E, nu, ampli, timecol="Relative time"):
 
     if 'keymap.back' in plt.rcParams:
         plt.rcParams['keymap.back'].remove('left')
@@ -41,7 +40,6 @@ def plateplot(x_gauges, E, nu, ampli, file, timecol="Relative time"):
 
     fig, (ax, axBB) = plt.subplots(nrows=2)
     ax.set_xlim((0, 250))
-    fig.canvas.manager.set_window_title(Path(file).stem)
     title = (
         "Index: {}, Step: {}\n"
         r"advance $\rightarrow$       rewind $\leftarrow$""\n"
@@ -49,49 +47,44 @@ def plateplot(x_gauges, E, nu, ampli, file, timecol="Relative time"):
         r"step += 1000 $\Uparrow$      step-=1000$\Downarrow$"
     )
     # Read file
-    df = read(file, sep=";", skiprows=list(range(7))+[8])
     time = df[timecol]
 
-    # Apply filter
-    df = lowfilter(df, cutoff=5, N=2)
-    # merged MeanBrother plot
-    strains = straindf(df, (45, 90, 135), ampli)
-    stress = stressdf(strains, E, nu)
-
-    stress_avg = average_rosette(stress)
-    gauge_cols = stress_avg.columns[1:]
-    for lab, col in zip(("//", "⊥", "xy"), gauge_cols):
-        axBB.plot(time, stress_avg[col]/1e6, label=lab)
+    df_avg = average_rosette(df)
+    gauge_cols_avg = df_avg.columns
+    gauge_cols_avg = gauge_cols_avg.drop(timecol)
+    for lab, col in zip(("//", "⊥", "xy"), gauge_cols_avg):
+        axBB.plot(time, df_avg[col], label=lab)
     axBB.set_xlabel("Time [s]")
-    axBB.set_ylabel(r"$\sigma$ [MPa]")
     vline = axBB.axvline(time[0], ls='-.', c='none', alpha=0.6)
     # axBB.legend(loc="upper left")
 
     # Transpose to have one list per direction
-    gauge_cols = triads(stress.columns[1:])
-    dir_cols = [list(columns) for columns in zip(*gauge_cols)]
+    dir_cols = [list(columns) for columns in zip(*triads(df.columns[1:]))]
     # Initiate points plot
     points = []
     for lab, cols in zip(("//", "⊥", "xy"), dir_cols):
-        stresses_i = stress[cols].iloc[0]
-        points_i, = ax.plot(x_gauges, stresses_i, '-o', mfc='w', label=lab)
+        df_i = df[cols].iloc[0]
+        points_i, = ax.plot(x_gauges, df_i, '-o', mfc='w', label=lab)
         points.append(points_i)
 
     ax.set_title(title.format(keys['index'], keys['step']))
     ax.set_xlabel("Position on plate")
-    ax.set_ylabel(rf"$\sigma(t = {time[keys['index']]:.2e} s)$ [MPa]")
-    ax.dataLim.y0 = stress.to_numpy().min()
-    ax.dataLim.y1 = stress.to_numpy().max()
+    ax.set_ylabel(rf"$t = {time[keys['index']]:.2e}$ s")
+
+    gauge_cols = df.columns
+    gauge_cols = gauge_cols.drop(timecol)
+    ax.dataLim.y0 = df[gauge_cols].to_numpy().min()
+    ax.dataLim.y1 = df[gauge_cols].to_numpy().max()
     ax.legend(loc="upper right")
 
     # Add key events
     def move_vlines():
         keys["index"] = max(0, min(keys["index"], time.size-1))
         for i, point_cols in enumerate(dir_cols):
-            points[i].set_data(x_gauges, stress[point_cols].iloc[keys["index"]])
+            points[i].set_data(x_gauges, df[point_cols].iloc[keys["index"]])
         vline.set_xdata(np.full_like(vline.get_xdata(), time[keys["index"]]))
         vline.set(color='gray')
-        ax.set_ylabel(rf"$\sigma(t = {time[keys['index']]:.2e} s)$")
+        ax.set_ylabel(rf"$t = {time[keys['index']]:.2e}$ s")
         # ax.relim()
         # ax.autoscale_view()
     def onkey(event):
@@ -108,11 +101,10 @@ def plateplot(x_gauges, E, nu, ampli, file, timecol="Relative time"):
     fig.canvas.mpl_connect('key_press_event', onkey)
     fig.canvas.mpl_connect('button_press_event', onclick)
 
-    plt.tight_layout()
-    plt.show()
+    return fig, (ax, axBB)
 
 
-if __name__ == "__main__":
+def main():
     # Définition du problème
     x_gauges = np.array((2, 50, 123, 200, 248))
     E=2.59e9
@@ -123,4 +115,23 @@ if __name__ == "__main__":
     # files = ["data/240417/test2_000.csv"]
     with plt.style.context("ggplot"):
         for file in files:
-            plateplot(x_gauges, E, nu, ampli, file)
+
+            df = read(file, sep=";", skiprows=list(range(7))+[8])
+            df.drop([c for c in df.columns
+                     if c.removeprefix("Ch").isnumeric()
+                     and int(c.removeprefix("Ch")) % 4 == 0])
+
+            # Apply filter
+            df = lowfilter(df, cutoff=5, N=2)
+            # merged MeanBrother plot
+            strains = straindf(df, (45, 90, 135), ampli)
+            stress = stressdf(strains, E, nu)
+
+            fig, (ax, axBB) = plateplot(stress, x_gauges, E, nu, ampli)
+            axBB.set_ylabel(r"$\sigma$ [Pa]")
+            plt.tight_layout()
+            plt.show()
+
+
+if __name__ == "__main__":
+    main()
