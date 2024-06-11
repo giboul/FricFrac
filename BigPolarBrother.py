51696500d05c29bad34932bef347b95238e9a100
giboul <axel.giboulot@epfl.ch>
2024-06-11 16:37:05 +0200

    remove exit

--- BigPolarBrother.py
+++ BigPolarBrother.py
@@ -499,8 +499,7 @@ def main(E: float = 2.59e9, nu: float = 0.35, angles=(45, 90, 135), amplificatio
                              separator=";",
                              skip_rows=7,
                              skip_rows_after_header=1)
-            # exit()
-            tensiondf = lowfilter(tensiondf, cutoff=5, N=3)
+            tensiondf = lowfilter(tensiondf, cutoff=500, N=3)
 
             if False:
                 gauge_channels = None
